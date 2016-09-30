0b680f1d7a7a8c0e9eca75cf95cc853fba510893
GREYBCOL000 <GREYBCOL000@HASD.local>
2016-09-30 13:43:19 -0400

    first commit

---
+++ ErgsAndTerst/Revise/videos/index.php
@@ -0,0 +1,36 @@
+<?php
+
+header('Location: https://goo.gl/5F6kjz');
+
+?>
+<!DOCTYPE html>
+<html>
+
+  <head>
+    <link href="http://s3.amazonaws.com/codecademy-content/courses/ltp/css/shift.css" rel="stylesheet">
+    <title>ErgsAndTerst - Videos</title>
+    <link rel="stylesheet" href="http://s3.amazonaws.com/codecademy-content/courses/ltp/css/bootstrap.css">
+    <link rel="stylesheet" href="main.css">
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+  </head>
+
+  <body>
+    <div class="nav">
+      <div class="container tabs">
+        <ul class="pull-left leftNav">
+          <li><a href="http://steamcommunity.com/id/Ergs/" target="_blank">Steam</a></li>
+          <li><a href="https://goo.gl/fkK4B3" target="_blank">Channel</a></li>
+          <li><a href="http://ergsandterst.ga/videos/">Videos</a></li>
+        </ul>
+        <ul class="pull-right rightNav">
+          <li><a href="http://www.ergsandterst.ga/">Home</a></li>
+          <li><a href="http://www.ergsandterst.ga/resources/">Resources</a></li>
+          <li><a href="http://www.ergsandterst.ga/about/">About</a></li>
+        </ul>
+      </div>
+    </div>
+
+    <iframe src="http://www.youtube.com/embed/?listType=user_uploads&list=XxColing123xX" width="480" height="400"></iframe>                   
+    
+  </body>
+</html> 
