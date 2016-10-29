f08b7cc74feb23b881ddd3457d142218d7410390
ErgsAndTerst <coling1231999@hotmail.com>
2016-10-29 14:13:03 -0400

    Finished getGitRepos; moved table output location

--- html/dynamic-api/js/gitScripts.js
+++ html/dynamic-api/js/gitScripts.js
@@ -2,57 +2,34 @@ function getGitRepos() {
     // Makes a variable for the oauth token
     var oauthToken = '13ef1c4fa910124f11';
     var oauthToken2 = '2c96eb9daeae4bfeb364bf';
+    var oauth = "?access_token=" + oauthToken + oauthToken2;
     // Makes two variables that contain empty strings
     var userName = '';
     var repoName = '';
     var folder = '';
-    var other = '';
+    //var other = '';
     // Sets userName and repoName equal to the current value of the inputs
     userName = $('#user').val();
     repoName = $('#repo').val();
     folder = $('#folder').val();
-    other = $('#other').val();
+    //other = $('#other').val();
     var mainChecked = isChecked('mainToggle');
     var folderChecked = isChecked('folderToggle');
-    var otherChecked = isChecked('otherToggle');
+    //var otherChecked = isChecked('otherToggle');
     // This if, else if, else statement checks if all required inputs are given
-    if (!mainChecked && !folderChecked && !otherChecked) {
-        // If no radio buttons were checked, then output this error.
-        printError("No structure option was chosen! Please try again.");
-    } else if (folderChecked && isEmpty(folder)) {
-        // If the folder radio button was checked but the text input is empty, output this error.
-        printError("You checked the second option but did not input a folder name! Please try again.");
-    } else if (otherChecked && isEmpty(other)) {
-        // If the other radio button was checked but the text input is empty, output this error.
-        printError("You checked the third option but did not input a folder name! Please try again.");
-    } else if (isEmpty(userName) || isEmpty(repoName)) {
-        // If the username or repository name is blank, output this error.
-        printError('You did not enter a username or repository name! Please try again.');
-    } else if (mainChecked) {
+    if (mainChecked) {
         jQuery(document).ready(function($) {
             userName = $('#user').val();
             repoName = $('#repo').val();
             $.ajax({
-                url : "https://api.github.com/repos/" + userName + "/" + repoName + "/contents" + "?access_token=" + oauthToken + oauthToken2,
+                url : "https://api.github.com/repos/" + userName + "/" + repoName + "/contents" + oauth,
                 dataType : 'jsonp',
                 success : function(mainDir) {
                     document.getElementById('repoStructure').style.display = 'block';
                     mainDirLength = mainDir.data.length;
                     console.log('mainDirLength: ' + mainDirLength);
                     console.log(mainDir);
-                    $.ajax ({
-                        url : "https://api.github.com/users/" + userName + "?access_token=" + oauthToken + oauthToken2,
-                        dataType : 'jsonp',
-                        success : function(userInfo) {
-                            console.log('userInfo:');
-                            console.log(userInfo);
-                            displayName = userInfo.data.login;
-                            document.getElementById('repoStructure').innerHTML = '<a href="https://www.github.com/'+ displayName + '" target="_blank">' + displayName + '</a>' + ' / ' + '<a href="https://www.github.com/'+ displayName + '/' + repoName + '" target="_blank">' + '<span class="bold">' + repoName + '</span>' + '</a>' + ' / ';
-                        },
-                        error : function() {
-                            printError('Something went wrong! Please try again.');
-                        }
-                    });
+                    getUserInfo(userName, repoName, oauth);
                     var urlList = '';
                     for (var i = 0; i < mainDirLength; i++) {
                         console.log('mainDir Iteration: ' + i);
@@ -65,7 +42,7 @@ function getGitRepos() {
                             document.getElementById('output').innerHTML = urlList;
                         };
                     }
-                    document.getElementById('hidden').style.display = 'block';
+                    document.getElementById('hidden').style.display = 'flex';
                     document.getElementById('error').style.display = 'none';
                 },
                 error : function() {
@@ -79,7 +56,7 @@ function getGitRepos() {
         folder = $('#folder').val();
         jQuery(document).ready(function($) {
             $.ajax({
-                url : "https://api.github.com/repos/" + userName + "/" + repoName + "/contents" + "?access_token=" + oauthToken + oauthToken2,
+                url : "https://api.github.com/repos/" + userName + "/" + repoName + "/contents" + oauth,
                 dataType : 'jsonp',
                 success : function(gitDir) {
                     console.log('gitDir:');
@@ -92,7 +69,7 @@ function getGitRepos() {
                         // IF the specific object is a directory with size 0 and a name of folder, call the API again
                         if (gitDir.data[i].type == 'dir' && gitDir.data[i].size == 0 && gitDir.data[i].name == folder) {
                             $.ajax({
-                                url : gitDir.data[i]._links.git + "?access_token=" + oauthToken + oauthToken2,
+                                url : gitDir.data[i]._links.git + oauth,
                                 dataType : 'jsonp',
                                 success : function(folderDir) {
                                     console.log('folderDir:');
@@ -112,21 +89,9 @@ function getGitRepos() {
                                             document.getElementById('output').innerHTML = urlList;
                                         }
                                     };
-                                    $.ajax ({
-                                        url : "https://api.github.com/users/" + userName + "?access_token=" + oauthToken + oauthToken2,
-                                        dataType : 'jsonp',
-                                        success : function(userInfo) {
-                                            console.log('userInfo:');
-                                            console.log(userInfo);
-                                            displayName = userInfo.data.login;
-                                            document.getElementById('repoStructure').innerHTML = '<a href="https://www.github.com/'+ displayName + '" target="_blank">' + displayName + '</a>' + ' / ' + '<a href="https://www.github.com/'+ displayName + '/' + repoName + '" target="_blank">' + repoName + '</a>' + ' / ' + '<span class="bold">' + 'html' + '</span> / ';
-                                        },
-                                        error : function() {
-                                            printError('Something went wrong! Please try again.');
-                                        }
-                                    });
+                                    getUserInfo(userName, repoName, oauth);
                                     // Unhides the table once the information is entered
-                                    document.getElementById('hidden').style.display = 'block';
+                                    document.getElementById('hidden').style.display = 'flex';
                                     document.getElementById('error').style.display = 'none';
                                 },
                                 error : function() {
@@ -145,22 +110,43 @@ function getGitRepos() {
                 }
             });
         });
-    } else if (otherChecked && !isEmpty(other)) {
+    } /*else if (otherChecked && !isEmpty(other)) {
         userName = $('#user').val();
         repoName = $('#repo').val();
         other = $('#other').val();
+        dirStr = other.split('/');
+        dirStrLength = dirStr.length;
         jQuery(document).ready(function($) {
             $.ajax({
-                url : "https://api.github.com/repos/" + userName + "/" + repoName + "/contents" + "?access_token=" + oauthToken + oauthToken2,
+                url : "https://api.github.com/repos/" + userName + "/" + repoName + "/contents" + oauth,
                 dataType : 'jsonp',
                 success : function(otherDir) {
-
+                    console.log('otherDir:');
+                    console.log(otherDir);
+                    // for (var i = 0; i < otherDir.length -1; i++) {
+                    //     $.ajax({
+                    //         url :
+                    //     });
+                    // }
+                    getUserInfo(userName, repoName, oauth);
                 },
                 error : function() {
                     printError('Ajax just threw an error! Please try again.')
                 }
             });
         });
+    }*/ else if (!mainChecked && !folderChecked /*&& !otherChecked*/) {
+        // If no radio buttons were checked, then output this error.
+        printError("No structure option was chosen! Please try again.");
+    } else if (folderChecked && isEmpty(folder)) {
+        // If the folder radio button was checked but the text input is empty, output this error.
+        printError("You checked the second option but did not input a folder name! Please try again.");
+    } /*else if (otherChecked && isEmpty(other)) {
+        // If the other radio button was checked but the text input is empty, output this error.
+        printError("You checked the third option but did not input a folder name! Please try again.");
+    }*/ else if (isEmpty(userName) || isEmpty(repoName)) {
+        // If the username or repository name is blank, output this error.
+        printError('You did not enter a username or repository name! Please try again.');
     } else {
         printError('Uh oh, something unexpected happened!');
     }
@@ -186,101 +172,19 @@ function isChecked(radID) {
     return document.getElementById(radID).checked;
 }
 
-
-// console.log('gitDir:');
-// console.log(gitDir);
-// // This entire if, else if, else statement filters out possible API returns that do not include the wanted data.
-// // If the requested data is in the response, the code runs normally
-// if (typeof gitDir.data[0] == 'object') {
-//     document.getElementById('repoStructure').style.display = 'block';
-//     // Take length of API response data
-//     gitDirLength = (gitDir.data.length);
-//     for (var i = 0; i < gitDirLength; i++) {
-//         // IF the specific object is a directory with size 0 and a name of folder, call the API again
-//         if (gitDir.data[i].type == 'dir' && gitDir.data[i].size == 0 && gitDir.data[i].name == folder) {
-//             $.ajax({
-//                 url : gitDir.data[i].git_url + "?access_token=" + oauthToken,
-//                 dataType : 'jsonp',
-//                 success : function(folderDir) {
-//                     console.log('folderDir:');
-//                     console.log(folderDir);
-//                     // API call returns the file structure of the html folder in the repo
-//                     var folderDirLength = Object.keys(folderDir.data.tree).length;
-//                     var baseUrl = 'https://' + userName + '.github.io' + '/' + repoName + '/' + folder + '/' + "?access_token=" + oauthToken;
-//                     var urlList = '';
-//                     // Loops through JSON code and takes the path of the folders within "user/repo/html/"
-//                     for (var j = 0; j < folderDirLength; j++) {
-//                         // IF the item in the directory is a tree, log the name
-//                         if (folderDir.data.tree[j].type == "tree") {
-//                             currentDir = folderDir.data.tree[j].path;
-//                             // Appends the urlList string with table data tags which include a hyperlink to the GitHub pages site
-//                             urlList += '<tr><td class="tableLeft">' + '<a href="' + baseUrl + currentDir + '\/' + 'html' + '\/index.html" target="_blank">' + currentDir + '\/' + 'html\/index.html<\/a>' + '<\/td><td class="tableRight">' + currentDir + '<\/td></tr>';
-//                             // Outputs urlList to the HTML table
-//                             document.getElementById('output').innerHTML = urlList;
-//                         }
-//                     };
-//                     $.ajax ({
-//                         url : "https://api.github.com/users/" + userName + "?access_token=" + oauthToken,
-//                         dataType : 'jsonp',
-//                         success : function(userInfo) {
-//                             console.log('userInfo:');
-//                             console.log(userInfo);
-//                             displayName = userInfo.data.login;
-//                             document.getElementById('repoStructure').innerHTML = '<a href="https://www.github.com/'+ displayName + '" target="_blank">' + displayName + '</a>' + ' / ' + '<a href="https://www.github.com/'+ displayName + '/' + repoName + '" target="_blank">' + repoName + '</a>' + ' / ' + '<span class="bold">' + 'html' + '</span> / ';
-//                         },
-//                         error : function() {
-//                             alert('Something went wrong! Please try again.');
-//                         }
-//                     });
-//                     // Unhides the table once the information is entered
-//                     document.getElementById('hidden').style.display = 'block';
-//                     document.getElementById('error').style.display = 'none';
-//                 },
-//                 // If ajax throws an error, an alert box will appear
-//                 error : function() {
-//                     alert('Something went wrong! Please try again.');
-//                 }
-//             });
-//         }
-//         // Edit if else statement to remove error flickering before table is unhidden
-//         else {
-//             printError('Invalid repository information! Please try again.');
-//         }
-//     }
-// }
-// else if (isEmpty(userName) == true || isEmpty(repoName) == true) {
-//     printError('You entered an incorrect username or repository name! Please try again.');
-// }
-// else if (isEmpty(folder) == true && isEmpty(other) == true) {
-//     printError('You entered an invalid folder name! Please try again.');
-// }
-// // If there is a message in the API response, it knows something went wrong.
-// else if (typeof gitDir.data.message == 'string') {
-//     // If the message is "Not Found," then the repo name was incorrect
-//     if (gitDir.data.message == 'Not Found') {
-//         printError('Repository not found! Please try again.');
-//     }
-//     // If the message is "API rate limit exceeded," then an error is displayed
-//     else if (gitDir.data.message.substr(0, 23) == 'API rate limit exceeded') {
-//         printError('API Rate limit exceeded!');
-//     }
-//     // If some other message is sent, an ambiguous error is displayed
-//     else {
-//         printError('Something went wrong! Please try again.');
-//     }
-// }
-// // If an unaccounted-for response is given, an error is displayed
-// else {
-//     printError('Uh oh, something unexpected happened!');
-// }
-
-// if (typeof gitDir.data.message == 'string') {
-//     // If the
-//     if (gitDir.data.message == 'Not Found') {
-//         printError('Repository not found! Please try again.');
-//     } else if (gitDir.data.message.substr(0, 23) == 'API rate limit exceeded') {
-//         printError('API Rate limit exceeded!');
-//     } else {
-//         printError('Something went wrong! Please try again.');
-//     }
-// }
+function getUserInfo(userName, repoName, oauth) {
+    $.ajax ({
+        url : "https://api.github.com/users/" + userName + oauth,
+        dataType : 'jsonp',
+        success : function(userInfo) {
+            console.log('userInfo:');
+            console.log(userInfo);
+            displayName = userInfo.data.login;
+            document.getElementById('repoStructure').innerHTML = '<a href="https://www.github.com/'+ displayName + '" target="_blank">' + displayName + '</a>' + ' / ' + '<a href="https://www.github.com/'+ displayName + '/' + repoName + '" target="_blank">' + '<span class="bold">' + repoName + '</span>' + '</a>' + ' / ';
+            document.getElementById('repoStructure').style.display = 'block';
+        },
+        error : function() {
+            printError('Something went wrong! Please try again.');
+        }
+    });
+}
